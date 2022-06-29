5dc605de16d0bce3da595c19d5ec03eea3921e13
unknown <leonid1972application@gmail.com>
2022-06-29 11:28:39 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>liste products</main>
+    </div>
+  );
+}
+
+export default App;
